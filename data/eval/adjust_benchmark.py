71b758f97834d05fb418e8a91f3ec0734b8292c9
Thomas Buckley <tbuckley@tabwebsites.com>
2023-12-30 20:06:29 -0500

    added new trial, added prompting for closed questions to include yes or no as options

--- data/eval/adjust_benchmark.py
+++ data/eval/adjust_benchmark.py
@@ -11,10 +11,17 @@ def modify_json_keys_path_vqa(file_path, output_file_path):
         # Modify the keys as required
         new_entry['id'] = entry['qid']
         new_entry['image'] = entry['image_name']
-        new_entry['answer_type'] = entry['answer_type']
+
+        question = entry['question']
+
+        answer_type = entry['answer_type']
+        if answer_type == 'CLOSED':
+            question = question + ' (Answer with either "Yes" or "No").'
+
+        new_entry['answer_type'] = answer_type
 
         new_entry['conversations'] = [
-            {"from": "human", "value": str(entry['question'])},
+            {"from": "human", "value": question},
             {"from": "gpt", "value": str(entry['answer'])}
         ]
 
@@ -25,9 +32,6 @@ def modify_json_keys_path_vqa(file_path, output_file_path):
         json.dump(modified_data, file)
         file.write('\n')
 
-        # for entry in modified_data:
-        #     json.dump(entry, file)
-        #     file.write('\n')
 
 modify_json_keys_path_vqa('../VQA_RAD/VQA_RAD Dataset Public.json', '../VQA_RAD/vqa_rad_qa.json')
 
