d608c4e35224090c3e4da8073496e1c190c3ae36
Thomas Buckley <tbuckley@tabwebsites.com>
2024-01-02 23:26:53 -0500

    updated image challenge prompt script

--- data/eval/adjust_benchmark_image_challenge.py
+++ data/eval/adjust_benchmark_image_challenge.py
@@ -1,44 +1,53 @@
 import json
-from data.benchmark_image_challenge.prompts import prompt_multimodal_fixed, prompt_image_only_fixed, \
-    prompt_text_only_fixed
+
+from data.benchmark_image_challenge.prompts import prompt_multimodal_fixed, prompt_text_only_fixed, \
+  prompt_image_only_fixed, prompt_multimodal_fixed_min, prompt_text_only_fixed_min, prompt_image_only_fixed_min
 
 
-def modify_json_keys_image_challenge(file_path, output_file_path, prompt_type):
-    with open(file_path, 'r') as file:
-        data = json.load(file)
+def modify_json_keys_image_challenge(file_path, output_file_path, prompt_type, minimal=False):
+  if minimal:
+    prompt_mm = prompt_multimodal_fixed_min
+    prompt_text_only = prompt_text_only_fixed_min
+    prompt_image_only = prompt_image_only_fixed_min
+  else:
+    prompt_mm = prompt_multimodal_fixed
+    prompt_text_only = prompt_text_only_fixed
+    prompt_image_only = prompt_image_only_fixed
 
-    modified_data = []
-    for entry in data:
-        new_entry = {}
-        new_entry['id'] = entry['date']
+  with open(file_path, 'r') as file:
+    data = json.load(file)
 
-        question = entry['question']
-        options = list(map(lambda x: x['label'], entry['options']))
+  modified_data = []
+  for entry in data:
+    new_entry = {'id': entry['date']}
 
-        if prompt_type == 'mm_fixed':
-            formatted_question = prompt_multimodal_fixed.format(question=question, options=options)
-            new_entry['image'] = entry['image_path'].split('\\')[1]
-        elif prompt_type == 'image_only_fixed':
-            formatted_question = prompt_image_only_fixed.format(options=options)
-            new_entry['image'] = entry['image_path'].split('\\')[1]
-        elif prompt_type == 'text_only_fixed':
-            # we just don't add an image key for the model to not include this in context
-            formatted_question = prompt_text_only_fixed.format(question=question, options=options)
-        else:
-            print('Please select one of: \'mm_fixed\', \'image_only_fixed\', or \'text_only_fixed\'')
-            return
+    question = entry['question']
+    options = list(map(lambda x: x['label'], entry['options']))
 
-        new_entry['conversations'] = [
-            {"from": "human", "value": formatted_question},
-            {"from": "gpt", "value": str(entry['answer'])}
-        ]
+    if prompt_type == 'mm_fixed':
+      formatted_question = prompt_mm.format(question=question, options=options)
+      new_entry['image'] = entry['image_path'].split('\\')[1]
+    elif prompt_type == 'image_only_fixed':
+      formatted_question = prompt_image_only.format(options=options)
+      new_entry['image'] = entry['image_path'].split('\\')[1]
+    elif prompt_type == 'text_only_fixed':
+      # we just don't add an image key for the model to not include this in context
+      formatted_question = prompt_text_only.format(question=question, options=options)
+    else:
+      print('\'prompt_type\' must be one of: \'mm_fixed\', \'image_only_fixed\', or \'text_only_fixed\'.')
+      return
 
-        modified_data.append(new_entry)
+    new_entry['conversations'] = [
+      {"from": "human", "value": formatted_question},
+      {"from": "gpt", "value": str(entry['answer'])}
+    ]
 
-    # Write the modified data to a JSON lines file
-    with open(output_file_path, 'w') as file:
-        json.dump(modified_data, file)
-        file.write('\n')
+    modified_data.append(new_entry)
+
+  # Write the modified data to a JSON lines file
+  with open(output_file_path, 'w') as file:
+    json.dump(modified_data, file)
+    file.write('\n')
 
 
 # generate the image challenge benchmark
@@ -54,4 +63,17 @@ modify_json_keys_image_challenge('../benchmark_image_challenge/benchmark_high_re
                                  '../benchmark_image_challenge/benchmark_high_res_text_only.json',
                                  'text_only_fixed')
 
+modify_json_keys_image_challenge('../benchmark_image_challenge/benchmark_high_res.json',
+                                 '../benchmark_image_challenge/benchmark_high_res_multimodal_min.json',
+                                 'mm_fixed',
+                                 minimal=True)
 
+modify_json_keys_image_challenge('../benchmark_image_challenge/benchmark_high_res.json',
+                                 '../benchmark_image_challenge/benchmark_high_res_image_only_min.json',
+                                 'image_only_fixed',
+                                 minimal=True)
+
+modify_json_keys_image_challenge('../benchmark_image_challenge/benchmark_high_res.json',
+                                 '../benchmark_image_challenge/benchmark_high_res_text_only_min.json',
+                                 'text_only_fixed',
+                                 minimal=True)
